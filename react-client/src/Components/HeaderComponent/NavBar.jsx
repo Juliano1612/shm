d251cb4dd4e17b40027800617f6e9a9863f43a43
Gregório Granado Magalhães <greg.magalhaes@gmail.com>
2018-04-26 17:43:38 -0300

    Nav

--- react-client/src/Components/HeaderComponent/NavBar.jsx
+++ react-client/src/Components/HeaderComponent/NavBar.jsx
@@ -5,9 +5,10 @@ class NavBar extends Component {
   render() {
     return (
         <div className="ui secondary pointing menu">
-            <Link to="/" className="item active"> Home </Link>
+            <Link to="/" className="item active"> Home</Link>
             <Link to="/about" className="item">About</Link>
             <Link to="/timeline" className="item">Timeline</Link>
+            <Link to="/comochegar" className="item">Como Chegar</Link>
         </div>
     )
   }
